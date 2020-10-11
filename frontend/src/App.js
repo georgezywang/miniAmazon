d2f8d351669b5b3ffda0dd0a05c41bc1deeb10c4
georgezywang <georgezywang@outlook.com>
2020-10-10 20:17:32 -0400

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import './App.css';
+import HomeScreen from './Screens/HomeScreen';
+import ProductScreen from './Screens/ProductScreen';
+
+function App() {
+
+  const openMenu = () => {
+    document.querySelector(".sidebar").classList.toggle("open");
+  }
+
+  return (
+    <BrowserRouter>
+      <div className="grid-container">
+
+        <header className="header">
+        <div className="brand">
+
+            <button onClick={openMenu}>
+              &#9776;
+            </button>
+            
+            <Link to="/" >miniAmazon</Link>
+          </div>
+          <div className="header-links">
+            <a href="cart.html">Cart</a>
+            <a href="signin.html">Sign In</a>
+          </div>
+        </header>
+
+        <aside className="sidebar">
+          <h3>Shopping Categories</h3>
+          <ul>
+            <li>
+              <a href="index.html">Pants</a>
+            </li>
+            <li>
+              <a href="index.html">Shirts</a>
+            </li>
+          </ul>
+        </aside>
+
+        <main className="main">
+          <div className="content">
+            <Route path="/product/:id" component={ProductScreen} />
+            <Route path="/" exact = {true} component={HomeScreen} />
+          </div>
+
+        </main>
+        <footer className="footer">
+          All right reserved.
+    </footer>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
